f7dc8713762bdd49600600e64bc5a3499e5ee6b7
Endscape <mishenka228339@gmail.com>
2026-01-05 16:07:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ const config = {
                 url: "https://mega.nz/file/ваша_ссылка",
                 type: "direct",
                 colorClass: "btn-alternative"
-            },
+            }
         ]
     },
     
